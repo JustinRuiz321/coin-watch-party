a5b6b30ce9821d8c3f003b831160553b4c7e752f
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-10-03 20:41:08 -0400

    Changes to some routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,13 +6,35 @@ router.get('/', async (req , res) => {
 
 });
 
+router.get('/login', (req,res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+})
+
 router.get('/crypto' , async (req , res) => {
 
 });
 
 router.get('/profile' , withAuth, async (req , res) => {
+   try {
+       const userData = await User.findByPk(req.session.user_id, {
+           attributes: {exclude: ['password']} ,
+           include: [{model: Project}],
+       });
+
+    const user = userData.get({ plain: true});
+
+    res.render('profile', {
+        ...user,
+        logged_in: true
+    });
+   } catch (err) {
+       res.status(500).json(err);
+   }
 
 });
-
-
 module.exports = routers;
