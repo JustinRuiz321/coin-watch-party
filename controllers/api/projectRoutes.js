02cb7d3899af42c892a304bfc60aabb5052defca
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-10-06 15:32:22 -0400

    cleaning

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -2,39 +2,5 @@ const router = require('express').Router();
 const { Coins } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// Create Coin
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newCoin = await Coins.create({
-      name: req.body.name,
-      description: req.body.description,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newCoin);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const coinData = await   Coins.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!coinData) {
-      res.status(404).json({ message: 'No project found with this id!' });
-      return;
-    }
-
-    res.status(200).json(projectData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 module.exports = router;
