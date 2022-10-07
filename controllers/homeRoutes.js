0c4f807967cb8ef935c77927087071a8b02816dc
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-10-06 22:50:01 -0400

    cleaning

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,8 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/coin-list', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const coinData = await Project.findAll({
       include: [
         {
           model: User,
@@ -14,12 +13,10 @@ router.get('/coin-list', async (req, res) => {
       ],
     });
 
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const coins = coinData.map((project) => project.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      coins, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -29,7 +26,6 @@ router.get('/coin-list', async (req, res) => {
 
 
 router.get('/', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/coin-list');
     return;
