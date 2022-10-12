0a45695a66e2fa15e8d1035ca0b10e38e6041fd8
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-10-12 19:38:26 -0400

    Fixing branch

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require('path');
+cconst path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
@@ -11,7 +11,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -26,7 +25,6 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -39,3 +37,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
