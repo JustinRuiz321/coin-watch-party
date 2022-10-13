56604a63227d2369ccdfe1c83de0b019b3806618
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-10-12 21:43:08 -0400

    Fixing branch

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/coin-list', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const coinData = await Project.findAll({
       include: [
         {
           model: User,
@@ -14,12 +13,10 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const coins = coinData.map((project) => project.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      coins, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -27,56 +24,18 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
 
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+router.get('/', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/coin-list');
     return;
   }
 
   res.render('login');
 });
 
+
+router.get('/signup' , (req, res) => {
+  res.render('signup');
+})
 module.exports = router;
